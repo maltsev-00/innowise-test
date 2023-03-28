c1efc0c9506388aac2f9e9bd62dd45e747a87b44
Dzmitry Maltsau (Ext) <dzmitry.maltsau@tietoevry.com>
2023-03-29 00:35:02 +0300

    add comment in SwaggerConfig.java

--- src/main/java/com/innowise/test/config/SwaggerConfig.java
+++ src/main/java/com/innowise/test/config/SwaggerConfig.java
@@ -9,7 +9,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
-
+// add comment
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
